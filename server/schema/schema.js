eb394a16958a34482790a6d94807b29cc9224a60
nbr41to <nbr.41to@gmail.com>
2021-01-11 10:11:25 +0900

    逆引きが可能になるList

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const _ = require('lodash');
+const _ = require('lodash')
 
 const {
   GraphQLObjectType,
@@ -7,21 +7,24 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
+  GraphQLList
 } = graphql
 
 // dummy data
 const books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1a' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2a' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2a' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3a' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3a' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3a' },
 ]
 const authors = [
   { name: 'Patrick Rothfuss', age: 44, id: '1a' },
   { name: 'Brandon Sanderson', age: 42, id: '2a' },
-  { name: 'Terry Pratchett', age: 66, id: '3a' },
+  { name: 'Terry Pratchett', age: 66, id: '3a' }
 ]
 
-// これがスキーマ？
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -31,25 +34,40 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        console.log(parent) // 検索したidのbooksのデータがparent
         return _.find(authors, { id: parent.authorId })
-        // そのparentのauthorIdと一致するauthorのデータを取得
       }
     }
   })
-});
-// fieldsは関数で返す
+})
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id })
+      }
+    }
   })
-});
+})
 
-// ルートクエリを作成
+// これらのクエリーで著者IDに一致する書籍を検索できる
+// {
+//   author(id: "3a") {
+//     name
+//     age
+//     books{
+//       id
+//       name
+//       genre
+//     }
+//   }
+// }
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -58,27 +76,19 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
-        return _.find(books, { id: args.id });
+        return _.find(books, { id: args.id })
       }
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        return _.find(authors, { id: args.id })
       }
-    },
+    }
   }
-});
-
-// 実際のクエリ
-// book(id:'123'){
-//   name
-//   genre
-// }
+})
 
 module.exports = new GraphQLSchema({
   query: RootQuery
-});
-
-
+})
